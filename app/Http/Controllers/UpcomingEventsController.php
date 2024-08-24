ea48c0fe081cf1d33fd49dd7c8093656fe9876b4
samuelavike1 <samuelavike@gmail.com>
2024-08-24 08:36:43 +0000

    models,migrations,controllers added

---
+++ app/Http/Controllers/UpcomingEventsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UpcomingEvents;
+use Illuminate\Http\Request;
+
+class UpcomingEventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UpcomingEvents $upcomingEvents)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UpcomingEvents $upcomingEvents)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UpcomingEvents $upcomingEvents)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UpcomingEvents $upcomingEvents)
+    {
+        //
+    }
+}
